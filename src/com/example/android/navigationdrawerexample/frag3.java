c051737f30e67a24aa983242c0cf49982005d5d8
Stephen Pettus <cs9stephen@gmail.com>
2013-08-18 20:25:24 -0400

    first commit

---
+++ src/com/example/android/navigationdrawerexample/frag3.java
@@ -0,0 +1,18 @@
+package com.example.android.navigationdrawerexample;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Stephen on 8/14/13.
+ */
+public class frag3 extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.frag3, null);
+
+    }
+}
